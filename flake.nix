245a1b34de6534129f80d113bf7fba9720b6b83b
Rui <rjfcasaleiro@gmail.com>
2025-12-06 20:32:12 +0000

    chore: add flake.nix for direnv Go development environment

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "GitHub MCP Server - Go 1.24 development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go_1_24
+            golangci-lint
+            git
+            git-lfs
+          ];
+
+          shellHook = ''
+            echo "GitHub MCP Server - Go 1.24 environment loaded"
+            go version
+            golangci-lint version
+          '';
+        };
+      }
+    );
+}
